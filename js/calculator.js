13697d7e17f672e2b967d02db78018fd5dea032c
michael <mgebregziabher86@gmail.com>
2017-08-28 15:44:19 -0500

    calculator second step finished withe operators and getting results

--- js/calculator.js
+++ js/calculator.js
@@ -15,7 +15,7 @@ for (var i = 0; i < numberBtns.length; i++) {
     numberBtns[i].addEventListener("click", function () {
         console.log("middle display" + middleDisplay.value);
 
-        if (rightDisplay.value === "" && middleDisplay.value === "" ) {
+        if (rightDisplay.value === "" && middleDisplay.value === "") {
             // exactly the same but with rightDisplay;
             var oldNumber = leftDisplay.getAttribute("value");
             if (oldNumber == null) {
@@ -26,24 +26,47 @@ for (var i = 0; i < numberBtns.length; i++) {
             leftDisplay.setAttribute("value", newValue);
             console.log(numberBtns);
 
-        } else if(leftDisplay.value !== "" && middleDisplay.value !== ""){
+        } else if (leftDisplay.value !== "" && middleDisplay.value !== "") {
             var oldNumber = rightDisplay.getAttribute("value");
-                if (oldNumber == null) {
-                    oldNumber = "";
-                }
-                var newValue = oldNumber + this.getAttribute("value");
-                console.log(newValue);
-                rightDisplay.setAttribute("value", newValue);
-                console.log(numberBtns);
+            if (oldNumber == null) {
+                oldNumber = "";
+            }
+            var newValue = oldNumber + this.getAttribute("value");
+            console.log(newValue);
+            rightDisplay.setAttribute("value", newValue);
+            console.log(numberBtns);
         }
     });
 }
-    for (var i = 0; i < operatorBtns.length; i++) {
-        operatorBtns[i].addEventListener("click", function () {
-            middleDisplay.setAttribute("value", this.getAttribute("value"));
-            console.log(operatorBtns);
-        });
 
-
-
+for (var i = 0; i < operatorBtns.length; i++) {
+    operatorBtns[i].addEventListener("click", function () {
+        middleDisplay.setAttribute("value", this.getAttribute("value"));
+        console.log(operatorBtns);
+    });
 }
+equals.addEventListener("click", function () {
+    var leftValue = parseInt(leftDisplay.getAttribute("value"));
+    var operator = middleDisplay.getAttribute("value");
+    var rightValue =  parseInt(rightDisplay.getAttribute("value"))
+    var total;
+
+    switch (operator){
+        case "+":
+            total = leftValue + rightValue;
+            break;
+        case "-":
+            total = leftValue - rightValue;
+            break;
+        case "/":
+            total = leftValue / rightValue;
+            break;
+        case "X":
+            total = leftValue * rightValue;
+            break;
+    }
+    console.log(total);
+    leftDisplay.setAttribute("value", total);
+    rightDisplay.setAttribute("value","");
+    middleDisplay.setAttribute("value","");
+})
