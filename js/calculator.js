a8fd351d9b036d93cffef35ee53de60f928d113f
michael <mgebregziabher86@gmail.com>
2017-08-28 14:10:31 -0500

    calculator second step

---
+++ js/calculator.js
@@ -0,0 +1,49 @@
+// function name(calculator) {
+
+var numberBtns = document.getElementsByClassName("number");
+var operatorBtns = document.getElementsByClassName("operator");
+var clear = document.getElementById("clear");
+var equals = document.getElementById("equals");
+
+var leftDisplay = document.getElementById("num1");
+var middleDisplay = document.getElementById("op");
+var rightDisplay = document.getElementById("num2");
+console.log(numberBtns);
+
+
+for (var i = 0; i < numberBtns.length; i++) {
+    numberBtns[i].addEventListener("click", function () {
+        console.log("middle display" + middleDisplay.value);
+
+        if (rightDisplay.value === "" && middleDisplay.value === "" ) {
+            // exactly the same but with rightDisplay;
+            var oldNumber = leftDisplay.getAttribute("value");
+            if (oldNumber == null) {
+                oldNumber = "";
+            }
+            var newValue = oldNumber + this.getAttribute("value");
+            console.log(newValue);
+            leftDisplay.setAttribute("value", newValue);
+            console.log(numberBtns);
+
+        } else if(leftDisplay.value !== "" && middleDisplay.value !== ""){
+            var oldNumber = rightDisplay.getAttribute("value");
+                if (oldNumber == null) {
+                    oldNumber = "";
+                }
+                var newValue = oldNumber + this.getAttribute("value");
+                console.log(newValue);
+                rightDisplay.setAttribute("value", newValue);
+                console.log(numberBtns);
+        }
+    });
+}
+    for (var i = 0; i < operatorBtns.length; i++) {
+        operatorBtns[i].addEventListener("click", function () {
+            middleDisplay.setAttribute("value", this.getAttribute("value"));
+            console.log(operatorBtns);
+        });
+
+
+
+}
